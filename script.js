1c91433d84f8a28e12d8d4a215a1e2c470cdc455
Leodominoni <95833042+Leodominoni@users.noreply.github.com>
2022-07-14 11:13:25 -0300

    up

---
+++ script.js
@@ -0,0 +1,17 @@
+//tipos de função
+//declarativas
+/*function funcao(){
+    console.log('Tá joia?');
+}
+funcao();
+//expressoes
+var funcao = function (){
+    console.log('Sou uma msg de expressão!');
+}
+funcao();
+//arrow
+var funcao = () => {
+    console.log('soy arrow!');
+}
+funcao()*/
+
